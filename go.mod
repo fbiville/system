8fbe1dbd65fd15b52baae6532bbb69f06578d8a4
Florent Biville <fbiville@pivotal.io>
2019-09-13 11:46:56 +0200

    [#66] Reconcile streaming processor

--- go.mod
+++ go.mod
@@ -7,10 +7,14 @@ require (
 	github.com/gogo/protobuf v1.2.1 // indirect
 	github.com/google/go-cmp v0.3.1
 	github.com/json-iterator/go v1.1.6 // indirect
+	github.com/knative/build v0.7.0
+	github.com/knative/pkg v0.0.0-20190912234432-34157973944a // indirect
+	github.com/mattbaird/jsonpatch v0.0.0-20171005235357-81af80346b1a // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/onsi/ginkgo v1.8.0
 	github.com/onsi/gomega v1.5.0
 	github.com/spf13/pflag v1.0.3 // indirect
+	go.uber.org/zap v1.9.1
 	golang.org/x/net v0.0.0-20190501004415-9ce7a6920f09 // indirect
 	golang.org/x/sys v0.0.0-20190429190828-d89cdac9e872 // indirect
 	golang.org/x/text v0.3.2 // indirect
@@ -18,5 +22,6 @@ require (
 	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
 	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
+	knative.dev/pkg v0.0.0-20190912234432-34157973944a // indirect
 	sigs.k8s.io/controller-runtime v0.2.0
 )
--- pkg/controllers/streaming/processor_controller.go
+++ pkg/controllers/streaming/processor_controller.go
@@ -18,36 +18,225 @@ package streaming
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/go-logr/logr"
+	"github.com/projectriff/system/pkg/apis/build/v1alpha1"
+	"github.com/projectriff/system/pkg/apis/streaming/v1alpha1/resources"
+	"github.com/projectriff/system/pkg/apis/streaming/v1alpha1/resources/names"
+	"github.com/projectriff/system/pkg/controllers/kmp"
+	"github.com/projectriff/system/pkg/tracker"
+	"go.uber.org/zap"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
+	buildv1alpha1 "github.com/projectriff/system/pkg/apis/build/v1alpha1"
 	streamingv1alpha1 "github.com/projectriff/system/pkg/apis/streaming/v1alpha1"
 )
 
 // ProcessorReconciler reconciles a Processor object
 type ProcessorReconciler struct {
 	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+	Log     logr.Logger
+	Scheme  *runtime.Scheme
+	Tracker tracker.Tracker
 }
 
 // +kubebuilder:rbac:groups=streaming.projectriff.io,resources=processors,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=streaming.projectriff.io,resources=processors/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 
 func (r *ProcessorReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("processor", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("processor", req.NamespacedName)
 
-	// your logic here
+	var original streamingv1alpha1.Processor
+	if err := r.Client.Get(ctx, req.NamespacedName, &original); err != nil {
+		return ctrl.Result{}, ignoreNotFound(err)
+	}
+
+	// Don't modify the informers copy
+	processor := original.DeepCopy()
+
+	// Reconcile this copy of the processor and then write back any status
+	// updates regardless of whether the reconciliation errored out.
+	result, err := r.reconcile(ctx, log, processor)
+
+	// check if status has changed before updating, unless requeued
+	if !result.Requeue && !equality.Semantic.DeepEqual(original.Status, processor.Status) {
+		if updateErr := r.Status().Update(ctx, processor); updateErr != nil {
+			log.Error(updateErr, "unable to update Processor status", "processor", processor)
+			return ctrl.Result{Requeue: true}, updateErr
+		}
+	}
+	return result, err
+}
+
+func (r *ProcessorReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	enqueueTrackedResources := &handler.EnqueueRequestsFromMapFunc{
+		ToRequests: handler.ToRequestsFunc(func(a handler.MapObject) []reconcile.Request {
+			requests := []reconcile.Request{}
+			for _, item := range r.Tracker.Lookup(a.Object.(metav1.ObjectMetaAccessor)) {
+				requests = append(requests, reconcile.Request{NamespacedName: item})
+			}
+			return requests
+		}),
+	}
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&streamingv1alpha1.Processor{}).
+		Owns(&appsv1.Deployment{}).
+		Watches(&source.Kind{Type: &buildv1alpha1.Function{}}, enqueueTrackedResources).
+		Complete(r)
+}
+
+func (r *ProcessorReconciler) reconcile(ctx context.Context, logger logr.Logger, processor *streamingv1alpha1.Processor) (ctrl.Result, error) {
+	if processor.GetDeletionTimestamp() != nil {
+		return ctrl.Result{}, nil
+	}
+
+	// We may be reading a version of the object that was stored at an older version
+	// and may not have had all of the assumed defaults specified.  This won't result
+	// in this getting written back to the API Server, but lets downstream logic make
+	// assumptions about defaulting.
+	processor.SetDefaults(ctx)
+
+	processor.Status.InitializeConditions()
+
+	// resolve function
+	functionName := types.NamespacedName{
+		Namespace: processor.Namespace,
+		Name:      processor.Spec.FunctionRef,
+	}
+	var function v1alpha1.Function
+	if err := r.Client.Get(ctx, functionName, &function); err != nil {
+		return ctrl.Result{Requeue: true}, ignoreNotFound(err)
+	}
+
+	processorName := types.NamespacedName{
+		Namespace: processor.GetNamespace(),
+		Name:      processor.GetName(),
+	}
+	if err := r.Tracker.Track(&function, processorName); err != nil {
+		return ctrl.Result{Requeue: true}, err
+	}
+	processor.Status.PropagateFunctionStatus(&function.Status)
+
+	inputAddresses, _, err := r.resolveStreams(ctx, processorName, processor.Spec.Inputs)
+	if err != nil {
+		return ctrl.Result{Requeue: true}, err
+	}
+	processor.Status.InputAddresses = inputAddresses
+	outputAddresses, outputContentTypes, err := r.resolveStreams(ctx, processorName, processor.Spec.Outputs)
+	if err != nil {
+		return ctrl.Result{Requeue: true}, err
+	}
+	processor.Status.OutputAddresses = outputAddresses
+	processor.Status.OutputContentTypes = outputContentTypes
+
+	deploymentName := types.NamespacedName{
+		Namespace: processor.GetNamespace(),
+		Name:      names.Deployment(processor),
+	}
+	var deployment *appsv1.Deployment
+	err = r.Client.Get(ctx, deploymentName, deployment)
+	if errors.IsNotFound(err) {
+		if deployment, err = r.createDeployment(ctx, processor); err != nil {
+			logger.Error(err, "Failed to create Deployment %q: %v", deploymentName, err)
+			return ctrl.Result{Requeue: true}, err
+		}
+	} else if err != nil {
+		logger.Error(err, "Failed to reconcile Processor: %q failed to Get Deployment: %q; %v", processor.Name, deploymentName, zap.Error(err))
+		return ctrl.Result{Requeue: true}, err
+	} else if !metav1.IsControlledBy(deployment, processor) {
+		// Surface an error in the processor's status,and return an error.
+		processor.Status.MarkDeploymentNotOwned(deploymentName.Name)
+		return ctrl.Result{}, fmt.Errorf("Processor: %q does not own Deployment: %q", processor.Name, deploymentName)
+	} else {
+		if deployment, err = r.reconcileDeployment(ctx, logger, processor, deployment); err != nil {
+			logger.Error(err, "Failed to reconcile Processor: %q failed to reconcile Deployment: %q; %v", processor.Name, deployment, zap.Error(err))
+			return ctrl.Result{Requeue: true}, err
+		}
+	}
+
+	// Update our Status based on the state of our underlying Deployment.
+	processor.Status.DeploymentName = deployment.Name
+	processor.Status.PropagateDeploymentStatus(&deployment.Status)
+	processor.Status.ObservedGeneration = processor.Generation
 
 	return ctrl.Result{}, nil
 }
 
-func (r *ProcessorReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&streamingv1alpha1.Processor{}).
-		Complete(r)
+func (r *ProcessorReconciler) reconcileDeployment(ctx context.Context, logger logr.Logger, processor *streamingv1alpha1.Processor, deployment *appsv1.Deployment) (*appsv1.Deployment, error) {
+	desiredDeployment, err := resources.MakeDeployment(processor)
+	if err != nil {
+		return nil, err
+	}
+
+	// Preserve replicas as is it likely set by an autoscaler
+	desiredDeployment.Spec.Replicas = deployment.Spec.Replicas
+
+	if deploymentSemanticEquals(desiredDeployment, deployment) {
+		// No differences to reconcile.
+		return deployment, nil
+	}
+	diff, err := kmp.SafeDiff(desiredDeployment.Spec, deployment.Spec)
+	if err != nil {
+		return nil, fmt.Errorf("failed to diff Deployment: %v", err)
+	}
+	logger.Info("Reconciling deployment diff (-desired, +observed): %s", diff)
+
+	// Don't modify the informers copy.
+	existing := deployment.DeepCopy()
+	// Preserve the rest of the object (e.g. ObjectMeta except for labels).
+	existing.Spec = desiredDeployment.Spec
+	existing.ObjectMeta.Labels = desiredDeployment.ObjectMeta.Labels
+
+	return existing, r.Client.Update(ctx, existing)
+}
+
+func (r *ProcessorReconciler) createDeployment(ctx context.Context, processor *streamingv1alpha1.Processor) (*appsv1.Deployment, error) {
+	deployment, err := resources.MakeDeployment(processor)
+	if err != nil {
+		return nil, err
+	}
+	if deployment == nil {
+		// nothing to create
+		return deployment, nil
+	}
+	return deployment, r.Client.Create(ctx, deployment)
+}
+
+func deploymentSemanticEquals(desiredDeployment, deployment *appsv1.Deployment) bool {
+	return equality.Semantic.DeepEqual(desiredDeployment.Spec, deployment.Spec) &&
+		equality.Semantic.DeepEqual(desiredDeployment.ObjectMeta.Labels, deployment.ObjectMeta.Labels)
+}
+
+func (r *ProcessorReconciler) resolveStreams(ctx context.Context, processorCoordinates types.NamespacedName, streamNames []string) ([]string, []string, error) {
+	var addresses []string
+	var contentTypes []string
+	for _, streamName := range streamNames {
+		streamName := types.NamespacedName{
+			Namespace: processorCoordinates.Namespace,
+			Name:      streamName,
+		}
+		var stream streamingv1alpha1.Stream
+		if err := r.Client.Get(ctx, streamName, &stream); err != nil {
+			return nil, nil, err
+		}
+		if err := r.Tracker.Track(&stream, processorCoordinates); err != nil {
+			return nil, nil, err
+		}
+		addresses = append(addresses, stream.Status.Address.String())
+		contentTypes = append(contentTypes, stream.Spec.ContentType)
+	}
+	return addresses, contentTypes, nil
 }
